04ade22bc02f7dc1ebda83cdc0312b76fc14a183
elera96 <laurawerling39@gmail.com>
2023-03-20 15:24:28 +0100

    imported html & css and adapted it to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className="App container-box" id="weather-app">
+
+
+
+ {/*search bar form */}
+
+    <form id="search-form">
+      <div className="form-group row">
+        <input type="text" className="search-city col-8" id="enteredCity"  placeholder="Search for my city 🔎" />
+        <input type="submit"
+        value="search"
+        className="col-3 location-search-button" />
+      </div>
+    </form>
+
+
+
+  <button className="current-location-btn" onclick="getLocation()">
+  Search for Current Location 
+  </button>
+
+  {/*current weather */}
+    <div className="current-weather-box">
+    <h1 className="searched-city">Munich</h1>
+    <p className="last_updated">Last updated:</p>
+    <p>
+      <span className="date" > 23.03.23 </span>
+    
+    <span className="time" id="current-time">04:04 </span>
+  </p>
+   <p className="current-temperature-total" >
+       <span className="current-temperature"> 4°
+    </span>
+    </p>
+
+    <hr/>
+    <ul>
+    <li className="weather-description" >sunny</li>
+
+    </ul>
+
+    <div className="weatherForecast"></div> 
+</div>
+<div className="impressum">
+  Open-source code by Laura on <a href="https://github.com/elera96/Vanilla_Weather_App" >Github</a>
+</div>
+</div>
+)
 }
 
-export default App;
+
